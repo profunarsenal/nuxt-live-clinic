48618422062bad3bcd0c165551ebcdf428f7041d
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-11-03 13:55:58 +0400

    Перенес store

--- store/index.js
+++ store/index.js
@@ -1,9 +1,32 @@
 export const state = () => ({
-    menuBurger: false
+    menuBurger: false,
+    scrollToSection: false
 })
 
 export const getters = {
     isOpenMenuBurger (state) {
         return state.menuBurger
+    },
+
+    needScrollToSection (state) {
+        return state.scrollToSection
+    }
+}
+
+export const mutations = {
+    closeMenu (state) {
+        state.menuBurger = false
+    },
+  
+    toggleMenu (state) {
+        state.menuBurger = !state.menuBurger
+    },
+  
+    onScroll (state) {
+        state.scrollToSection = true
+    },
+  
+    offScroll (state) {
+        state.scrollToSection = false
     }
 }
