8eeeb912bbc20bdd81b1653cd573bfe4a237107b
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-11-03 14:44:45 +0400

    Добавил секции

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,38 @@
 <template lang="pug">
   .home
+    main-screen
+    section-about
+    section-profile
+    section-methods
+    section-solutions
+    section-workers.container
+    section-entry
+    section-contacts.container
 </template>
 
 <script>
 import Vue from 'vue'
+import MainScreen from '@/components/pageHome/MainScreen'
+import SectionAbout from '@/components/pageHome/SectionAbout'
+import SectionProfile from '@/components/pageHome/SectionProfile'
+import SectionMethods from '@/components/pageHome/SectionMethods'
+import SectionSolutions from '@/components/pageHome/SectionSolutions'
+import SectionWorkers from '@/components/pageSpecialists/SectionWorkers'
+import SectionEntry from '@/components/pageHome/SectionEntry'
+import SectionContacts from '@/components/pageContacts/SectionContacts'
 
 export default Vue.extend({
     name: 'IndexPage',
 
     components: {
-        
+        MainScreen,
+        SectionAbout,
+        SectionProfile,
+        SectionMethods,
+        SectionSolutions,
+        SectionWorkers,
+        SectionEntry,
+        SectionContacts
     }
 })
 </script>
