241fe225b80da8fd0cbae22157d725bb79ba8233
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-11-03 11:31:00 +0400

    Добавил компонент v-icon

---
+++ components/common/VIcon.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+    svg.icon
+        use(:xlink:href="'icons/list.svg#' + icon")
+</template>
+
+<script>
+export default {
+    name: 'VIcon',
+
+    props: {
+        icon: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+.icon
+    width: 24px
+    height: 24px
+</style>
