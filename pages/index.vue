9f63e9901df21b0afe8ac66aa8c3485915776352
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-11-03 14:16:04 +0400

    Добавил структуру файлов

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,28 @@
 <template lang="pug">
-  .home {{$t('lang')}}
+  .home
 </template>
 
 <script>
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+    name: 'IndexPage',
+
+    components: {
+        
+    }
 })
 </script>
+
+<style lang="sass" scoped>
+.container
+  padding-bottom: 150px
+
+@media (max-width: 1366px)
+  .container
+    padding-bottom: 100px
+
+@media (max-width: 992px)
+  .container
+    padding-bottom: 72px
+</style>
