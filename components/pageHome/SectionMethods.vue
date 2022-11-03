70e2fd182dff2ecd8ee7a56d7b5e59fe20ae2e2c
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-11-03 15:22:38 +0400

    Добавил секцию

--- components/pageHome/SectionMethods.vue
+++ components/pageHome/SectionMethods.vue
@@ -1,9 +1,198 @@
 <template lang="pug">
-    div
+    .methods
+        .container
+            .methods-inner
+                h2.methods-title
+                    span.main-title {{ $t("methods.title") }}
+                    span.subtitle {{ $t("methods.subtitle") }}
+
+                v-icon.methods-icon(icon="arrow")
+
+                ul.methods-items
+                    li.methods-item(
+                        v-for="item in methods"
+                        :key="item.name"
+                    )
+                        h6.abbreviation {{ item.name }}
+                        p.name(v-html="item.title")
+                        p.description.description-hide {{ item.description }}
+
+                        .hideblock
+                            h6.abbreviation {{ item.name }}
+                            p.name(v-html="item.title")
+                            p.description {{ item.description }}
+
 </template>
 
 <script>
+import VIcon from '@/components/common/VIcon'
+
 export default {
-    name: 'SectionMethods'
+    name: 'SectionMethods',
+
+    components: {
+        VIcon
+    },
+
+    data () {
+        return {
+            methods: this.$t('methods.items')
+        }
+    }
 }
 </script>
+
+<style lang="sass" scoped>
+.methods
+  background-color: $color-whitesmoke
+
+.methods-inner
+  position: relative
+  padding: 150px 0
+
+.methods-title
+  @extend .title
+  display: flex
+  margin-bottom: 72px
+
+.main-title
+  margin-right: 72px
+
+.subtitle
+  max-width: 362px
+  padding-top: 12px
+  font-weight: 500
+  font-size: 18px
+  line-height: 155%
+  color: $color-darkgray
+
+.methods-icon
+  position: absolute
+  top: 0
+  right: 0
+  transform: rotate(180deg)
+  fill: $color-dark
+
+.methods-items
+  display: grid
+  grid-template-columns: repeat(6, 1fr)
+  grid-auto-columns: 1fr 1fr
+  gap: 16px
+
+.methods-item
+  position: relative
+  padding: 40px
+  border-radius: 20px
+  min-height: 230px
+  background-color: $color-white
+  cursor: pointer
+
+  @media (any-hover: hover)
+    &:hover
+      .hideblock
+        opacity: 1
+        visibility: visible
+
+      .description
+        transform: translateY(0)
+
+  &:nth-child(1)
+    grid-column: 1/3
+
+  &:nth-child(2)
+    grid-column: 3/5
+
+  &:nth-child(3)
+    grid-column: 5/7
+
+  &:nth-child(4)
+    grid-column: 1/4
+
+  &:nth-child(5)
+    grid-column: 4/7
+
+.abbreviation
+  margin-bottom: 16px
+  font-size: 64px
+  line-height: 110%
+  letter-spacing: -0.02em
+  color: $color-cyan
+
+.name
+  margin-bottom: 16px
+  font-size: 24px
+  line-height: 135%
+
+.hideblock
+  position: absolute
+  z-index: 5
+  top: 0
+  left: 0
+  padding: 40px
+  border-radius: 20px
+  background-color: $color-white
+  opacity: 0
+  visibility: hidden
+  box-shadow: 0px 32px 64px -12px rgba(16, 24, 40, 0.1)
+  transition: all 0.5s ease
+
+.description-hide
+  display: none
+
+.description
+  font-weight: 500
+  font-size: 18px
+  line-height: 155%
+  color: rgba($color-darkgray, 0.7)
+  transform: translateY(-10%)
+  transition: all 0.3s ease
+
+@media (max-width: 992px)
+  .methods-inner
+    padding: 100px 0
+
+  .methods-icon
+    display: none
+
+  .main-title
+    margin-right: 36px
+
+  .methods-item
+    padding: 24px
+
+  .abbreviation
+    font-size: 48px
+
+  .name
+    font-size: 18px
+
+@media (max-width: 767px)
+  .methods-inner
+    padding: 72px 0
+
+  .methods-title
+    flex-direction: column
+    align-items: center
+    text-align: center
+
+  .name
+    margin-bottom: 36px
+
+  .main-title
+    margin-right: 0
+    margin-bottom: 40px
+
+  .methods-items
+    display: flex
+    flex-direction: column
+
+  .methods-item
+    height: auto
+
+  .description-hide
+    display: block
+
+@media (max-width: 480px)
+  .name
+    margin-bottom: 56px
+</style>
--- components/pageHome/SectionSolutions.vue
+++ components/pageHome/SectionSolutions.vue
@@ -1,9 +1,145 @@
 <template lang="pug">
-    div
+    .container
+        .solutions-inner
+            h2.solutions-title {{ $t("solutions.title") }}
+
+            .solutions-grid
+                .director
+                    img.avatar(
+                        src="https://firebasestorage.googleapis.com/v0/b/liveclinic63.appspot.com/o/worker_1.jpg?alt=media&token=e6a4e3b9-013d-410a-81e3-cadba473d538"
+                        alt="avatar"
+                    )
+                    v-icon.solutions-icon(icon="arrow")
+                    span.director-name {{ $t("solutions.name") }}
+                    span.director-position {{ $t("solutions.position") }}
+                
+                ul.solutions-items
+                    li.solutions-item(
+                        v-for="item in solutions"
+                        :key="item.text"
+                    )
+                        v-icon.check(icon="check")
+                        p.description(v-html="item.text")
 </template>
 
 <script>
+import VIcon from '@/components/common/VIcon'
+
 export default {
-    name: 'SectionSolutions'
+    name: 'SectionSolutions',
+
+    components: {
+        VIcon
+    },
+
+    data () {
+        return {
+            solutions: this.$t('solutions.items')
+        }
+    }
 }
 </script>
+
+<style lang="sass" scoped>
+.solutions-title
+  @extend .title
+  margin-bottom: 72px
+
+.solutions-inner
+  padding: 150px 0
+
+.solutions-grid
+  display: grid
+  grid-template-columns: repeat(3, 1fr)
+
+.director
+  display: flex
+  flex-direction: column
+  margin-right: 48px
+
+.avatar
+  margin-bottom: 106px
+  width: 256px
+  height: 256px
+  object-fit: cover
+  border-radius: 50%
+
+.solutions-icon
+  margin-bottom: 32px
+  fill: $color-dark
+
+.director-name
+  margin-bottom: 12px
+  font-weight: 600
+  font-size: 24px
+  line-height: 135%
+
+.director-position
+  font-size: 18px
+  line-height: 155%
+  color: $color-darkgray
+
+.solutions-items
+  grid-column: 2/4
+  display: grid
+  grid-template-columns: 1fr 1fr
+  grid-template-rows: repeat(4, min-content)
+  column-gap: 48px
+  row-gap: 40px
+
+.check
+  margin-bottom: 12px
+  fill: $color-cyan
+
+.description
+  font-size: 18px
+  line-height: 155%
+
+@media (max-width: 992px)
+  .solutions-inner
+    padding: 100px 0
+
+  .avatar
+    margin-bottom: 50px
+    width: 226px
+    height: 226px
+
+  .solutions-items
+    column-gap: 42px
+    row-gap: 20px
+
+  .check
+    margin-bottom: 6px
+
+@media (max-width: 767px)
+  .solutions-inner
+    padding: 72px 0
+
+  .solutions-title
+    margin-bottom: 40px
+    text-align: center
+
+  .solutions-icon
+    display: none
+
+  .solutions-grid
+    display: flex
+    flex-direction: column
+
+  .avatar
+    width: 256px
+    height: 256px
+    margin: 0 auto 30px
+
+  .director
+    margin-right: 0
+    margin-bottom: 40px
+    text-align: center
+
+@media (max-width: 576px)
+  .solutions-items
+    grid-template-columns: 1fr
+    grid-template-rows: repeat(8, min-content)
+    column-gap: 0
+    row-gap: 40px
+</style>
