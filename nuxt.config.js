7285922e3c621c30ccd810e417f710d7abe43134
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-11-04 21:44:35 +0400

    Добавил плагин

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,8 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/axios',
-    '~/plugins/service'
+    '~/plugins/service',
+    { src: '~/plugins/swiper', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
