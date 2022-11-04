22163f6a2bb43ed53b2d0b7bcaa835024a8cc9ca
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-11-04 18:17:35 +0400

    Настроил страницу

--- pages/Info.vue
+++ pages/Info.vue
@@ -1,9 +1,49 @@
 <template lang="pug">
-    div
+    .container
+        section-questions(:spollers="spollers")
+        section-articles(:articles="articles")
+        section-contacts
 </template>
 
 <script>
+import SectionQuestions from '@/components/pageInfo/SectionQuestions'
+import SectionArticles from '@/components/pageInfo/SectionArticles'
+import SectionContacts from '@/components/pageContacts/SectionContacts'
+
 export default {
-    name: 'Info'
+    name: 'Info',
+
+    components: {
+        SectionQuestions,
+        SectionArticles,
+        SectionContacts
+    },
+
+    async fetch ({ store }) {
+        if (!store.getters['spollers/spollers'].length) {
+            await store.dispatch('spollers/getSpollers')
+        }
+    },
+
+    computed: {
+        spollers () {
+            return this.$store.getters['spollers/spollers']
+        }
+    },
+
+    mounted () {
+        this.setTitle()
+    },
+
+    methods: {
+        setTitle () {
+            document.title = this.$t('titles.info')
+        }
+    }
 }
 </script>
+
+<style lang="sass" scoped>
+.container
+    padding-top: 96px
+</style>
