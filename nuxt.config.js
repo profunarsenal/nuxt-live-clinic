c19d39fad00a5f670ef58d62a05d6de8398baa22
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-11-04 15:20:59 +0400

    Конфиг axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/axios',
+    '~/plugins/service'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -65,7 +67,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/'
+    baseURL: `https://liveclinic63-default-rtdb.firebaseio.com/${i18n.defaultLocale}`
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
