9fbf4534b281bc9b91e308086ba2c58053803b1d
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-11-04 19:34:52 +0400

    Добавил секцию

--- components/pageInfo/SectionArticles.vue
+++ components/pageInfo/SectionArticles.vue
@@ -1,9 +1,85 @@
 <template lang="pug">
-    div
+    .articles(v-if="articles.length")
+        .articles-header
+            h2.articles-title {{ $t("articles.title") }}
+
+            img.image(
+                src="@/assets/images/articles.svg"
+                alt="illustration"
+            )
+
+        .articles-list
+            card-article(
+                v-for="article in articles"
+                :key="article.id"
+                :article="article"
+            )
+
+    .error-message(v-else) {{ $t("articles.error") }}
 </template>
 
 <script>
+import CardArticle from '@/components/pagePaper/CardArticle'
+
 export default {
-    name: 'SectionArticles'
+    name: 'SectionArticles',
+
+    components: {
+        CardArticle
+    },
+
+    props: {
+        articles: {
+            type: Array,
+            default: () => []
+    }
+  }
 }
 </script>
+
+<style lang="sass" scoped>
+.articles
+    padding: 100px 0 150px
+
+.articles-header
+    position: relative
+
+.articles-title
+    @extend .title
+    margin-bottom: 72px
+
+.image
+    position: absolute
+    top: -100px
+    right: 0
+
+.articles-list
+    display: grid
+    justify-content: center
+    grid-template-columns: repeat(auto-fit, minmax(288px, 1fr))
+    row-gap: 72px
+    column-gap: 48px
+
+.error-message
+    padding: 100px 0
+    font-size: 24px
+
+@media (max-width: 1366px)
+    .articles
+        padding: 100px 0
+
+@media (max-width: 767px)
+    .articles
+        padding: 72px 0 20px
+
+    .articles-header
+        display: flex
+        flex-direction: column-reverse
+        align-items: center
+        justify-content: center
+
+    .image
+        position: relative
+        top: 0
+        margin-bottom: 40px
+</style>
---
+++ components/pagePaper/SectionArticle.vue
@@ -0,0 +1,153 @@
+<template lang="pug">
+    .section-article(v-if="article")
+        v-crumbs(
+            link="/info"
+            :lastPage="$t('articles.crumbs')"
+            :currentPage="article.title"
+        )
+
+        v-tag(:color="article.class") {{ article.tag }}
+
+        h2.article-title {{ article.title }}
+
+        .info
+            img.avatar(
+                :src="article.img"
+                alt="avatar"
+            )
+
+            .author
+                p.author-name {{ article.author }}
+                p.time
+                    span {{ article.date }} • {{ article.time }}
+                    span  {{ $tc("minutes", defineMinutes) }} {{ $t("articles.read") }}
+        
+        .content
+            .description {{ article.description }}
+
+            ul.description-list
+                li.description-item(
+                    v-for="item in article.articles"
+                    :key="item.title"
+                )
+                    h4.description-title {{ item.title }}
+                    p.description-text {{ item.description }}
+
+    .error-message(v-else) {{ $t("article.error") }}
+</template>
+
+<script>
+import pluralization from '@/mixins/pluralization'
+import VTag from '@/components/common/VTag'
+import VCrumbs from '@/components/common/VCrumbs'
+
+export default {
+    name: 'SectionArticle',
+
+    components: {
+        VTag,
+        VCrumbs
+    },
+
+    props: {
+        article: {
+            type: Object,
+            default: () => {}
+        }
+    },
+
+    mixins: [pluralization]
+}
+</script>
+
+<style lang="sass" scoped>
+.section-article
+    display: flex
+    flex-direction: column
+    align-items: flex-start
+    max-width: 742px
+    margin: 0 auto
+    padding: 88px 0 132px 0
+
+.article-title
+    @extend .title
+    margin-bottom: 24px
+
+.info
+    display: flex
+    align-items: center
+    margin-bottom: 64px
+
+.avatar
+    width: 56px
+    height: 56px
+    margin-right: 16px
+    border-radius: 50%
+    object-fit: cover
+    overflow: hidden
+
+.author
+    display: flex
+    flex-direction: column
+
+.author-name
+    margin-bottom: 4px
+    font-weight: 600
+    font-size: 18px
+    line-height: 155%
+
+.time
+    font-weight: 600
+    line-height: 150%
+    color: $color-lightgray
+
+.description
+    margin-bottom: 40px
+    font-weight: 500
+    font-size: 20px
+    line-height: 175%
+    color: $color-darkgray
+
+.description-item:not(:last-child)
+    margin-bottom: 40px
+
+.description-title
+    margin-bottom: 12px
+    font-weight: 700
+    font-size: 32px
+    line-height: 140%
+
+.description-text
+    font-weight: 500
+    font-size: 20px
+    line-height: 175%
+    color: $color-darkgray
+
+.error-message
+    padding: 100px 0
+    font-size: 24px
+
+@media (max-width: 1366px)
+    .section-article
+        padding: 40px 0 100px 0
+
+@media (max-width: 767px)
+    .section-article
+        padding: 0 0 72px 0
+        align-items: center
+
+    .article-title
+        text-align: center
+
+    .info
+        flex-direction: column
+        text-align: center
+
+    .avatar
+        margin-right: 0
+        margin-bottom: 16px
+
+    .description-title
+        font-size: 24px
+        line-height: 135%
+</style>
