ef77cc376b24bba3b3f0dbb5bdb16992e4c607b7
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-11-04 17:54:24 +0400

    Настроил страницу

--- pages/Services.vue
+++ pages/Services.vue
@@ -1,9 +1,46 @@
 <template lang="pug">
-    div
+    .container
+        section-price(:services="services")
+        section-contacts
 </template>
 
 <script>
+import SectionPrice from '@/components/pageServices/SectionPrice'
+import SectionContacts from '@/components/pageContacts/SectionContacts'
+
 export default {
-    name: 'Services'
+    name: 'Services',
+
+    components: {
+        SectionPrice,
+        SectionContacts
+    },
+
+    async fetch ({ store }) {
+        if (!store.getters['services/services'].length) {
+            await store.dispatch('services/getServices')
+        }
+    },
+
+    computed: {
+        services () {
+            return this.$store.getters['services/services']
+        }
+    },
+
+    mounted () {
+        this.setTitle()
+    },
+
+    methods: {
+        setTitle () {
+            document.title = this.$t('titles.services')
+        }
+    }
 }
 </script>
+
+<style lang="sass" scoped>
+.container
+    padding-top: 96px
+</style>
