adaf5f86542e39c69238851acf1ee294eb0a3172
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-11-04 19:35:17 +0400

    Изменил страницу

---
+++ pages/Paper.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+    .container
+        section-article(:article="article")
+        articles-slider(:articles="otherArticles")
+        section-contacts
+</template>
+
+<script>
+import SectionArticle from '@/components/pagePaper/SectionArticle'
+import SectionContacts from '@/components/pageContacts/SectionContacts'
+import ArticlesSlider from '@/components/pagePaper/ArticlesSlider'
+
+export default {
+    name: 'Paper',
+
+    components: {
+        SectionArticle,
+        SectionContacts,
+        ArticlesSlider
+    },
+
+    async fetch ({ store }) {
+        if (!store.getters['articles/articles'].length) {
+            await store.dispatch('articles/getArticles')
+        }
+    },
+
+    computed: {
+        otherArticles () {
+            return this.$store.getters['articles/otherArticles'](this.$route.query.id)
+        },
+
+        article () {
+            return this.$store.getters['articles/article'](this.$route.query.id)
+        }
+    },
+
+    mounted () {
+        this.setTitle()
+    },
+
+    methods: {
+        setTitle () {
+            const title = this.article?.title || this.$t('titles.article')
+            document.title = title
+        }
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+.container
+    padding-top: 96px
+</style>
