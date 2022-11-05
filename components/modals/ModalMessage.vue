67c01b3171ec3bb70a0a10694b9d048a79b6e79e
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-11-05 12:52:42 +0400

    Добавил модалку

--- components/modals/ModalMessage.vue
+++ components/modals/ModalMessage.vue
@@ -1,9 +1,49 @@
 <template lang="pug">
-    div
+    v-modal(
+        :isOpenModal="isOpenModal"
+        @close="$emit('close')"
+    )
+        .modal-message(v-if="message")
+            h6.message-title {{ message.title }}
+            p.message-text {{ message.text }}
 </template>
 
 <script>
+import VModal from '@/components/common/VModal'
+
 export default {
-    name: 'ModalMessage'
+    name: 'ModalMessage',
+
+    components: {
+        VModal
+    },
+
+    props: {
+        message: {
+            type: Object,
+            default: () => {}
+        },
+
+        isOpenModal: {
+            type: Boolean,
+            default: false
+        }
+    }
 }
 </script>
+
+<style lang="sass" scoped>
+.modal-message
+    padding: 30px
+    text-align: center
+
+.message-title
+    display: block
+    margin-bottom: 10px
+    font-size: 24px
+    font-weight: 600
+
+.message-text
+    font-size: 18px
+    line-height: 140%
+</style>
