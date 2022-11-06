9ffca29212d5f92240b6e693c4b6c5dffe0ed5de
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-11-06 14:42:33 +0400

    Убрал префикс

--- locales/index.js
+++ locales/index.js
@@ -6,6 +6,7 @@ const languages = { en, ru }
 export default {
   locales: ['en', 'ru'],
   defaultLocale: 'ru',
+  strategy: 'no_prefix',
   vueI18n: {
     fallbackLocale: 'ru',
     messages: Object.assign(languages)
