1e26a76bf35b0eac4535cdc8b5b1983bb4fd33f5
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-11-07 14:49:46 +0400

    Добавил страницу 404

---
+++ layouts/empty.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+    .empty
+        Nuxt
+</template>
+
+<script>
+export default {
+    name: 'Empty'
+}
+</script>
+
+<style lang="sass" scoped>
+.empty
+    display: flex
+    align-items: center
+    justify-content: center
+    min-height: 100%
+    min-width: 320px
+    padding: 0 15px
+    font-family: "Manrope", sans-serif
+    font-size: 16px
+    color: $color-dark
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+    .error
+        img(
+            src="@/assets/images/404.jpg"
+            class="error-image"
+        )
+
+        v-button.button-error(
+            color="primary"
+            @click="$router.push('/')"
+        ) {{ $t("buttons.main") }}
+</template>
+
+<script>
+import VButton from '@/components/common/VButton'
+
+export default {
+    components: {
+        VButton
+    },
+
+    layout: 'empty'
+}
+</script>
+
+<style lang="sass" scoped>
+.error
+    max-width: 600px
+
+.error-image
+    width: 100%
+
+.button-error
+    margin: 0 auto
+</style>
