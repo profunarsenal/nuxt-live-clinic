877ffaae7d58a363edce221838067798e8dbdad7
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-11-11 12:23:25 +0400

    Установка doteenv

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,8 @@ export default {
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     '@nuxtjs/style-resources',
-    '@nuxtjs/composition-api/module'
+    '@nuxtjs/composition-api/module',
+    '@nuxtjs/dotenv'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
